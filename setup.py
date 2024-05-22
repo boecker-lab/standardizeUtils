1e2bc7c2ca63f41f1ffe97f9a37120bfc30fd9d1
ju52les <nils.alexander.haupt@uni-jena.de>
2024-05-22 16:43:23 +0200

    Setting up repository

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='standardizeUtils',
+      version='0.1',
+      description='Utilities for standardizing SMILES and InChI strings with PubChem or rdkit',
+      url='',
+      author='Nils Haupt',
+      packages=['standardizeUtils'])
