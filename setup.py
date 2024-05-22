11a09d082adcf7d0ebf0f5e8341514df8cfe3ad9
ju52les <nils.alexander.haupt@uni-jena.de>
2024-05-22 16:45:32 +0200

    Added github url

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(name='standardizeUtils',
       version='0.1',
       description='Utilities for standardizing SMILES and InChI strings with PubChem or rdkit',
-      url='',
+      url='https://github.com/boecker-lab/standardizeUtils/',
       author='Nils Haupt',
       packages=['standardizeUtils'])
