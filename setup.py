d2cfdc1c61d534b987d32cd8bc7186a71a4b1f8d
ju52les <nils.alexander.haupt@uni-jena.de>
2024-10-21 12:47:31 +0200

    Removed setup.py and added pyproject.toml instead

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='standardizeUtils',
-      version='0.1',
-      description='Utilities for standardizing SMILES and InChI strings with PubChem or rdkit',
-      url='https://github.com/boecker-lab/standardizeUtils/',
-      author='Nils Haupt',
-      packages=['standardizeUtils'],
-     install_requires=['requests'])
